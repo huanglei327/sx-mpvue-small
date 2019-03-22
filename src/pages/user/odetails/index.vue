df6e7e1f28bd8093887c2fcb4c69b6adac16a5e2
huanglei327 <huanglei173@qq.com>
2019-03-22 18:00:40 +0800

    2019-03-22

--- src/pages/user/odetails/index.vue
+++ src/pages/user/odetails/index.vue
@@ -1,17 +1,83 @@
 <template>
-  <div style="width:100%;">
-   <div class="c-address">
+  <div class="container">
+    <div class="c-address">
       <van-cell-group>
-        <van-cell icon="location" is-link label="收货地址：广东省深圳市南山区百里大厦" @click="upAddress">
+        <van-cell icon="location">
           <div slot="title">
-            <div v-if="userName">
-              <div>收货人 : {{userName}} {{telNumber}}</div>
-              <div class="c-add-ils color6">收货地址：{{address}}</div>
+            <div v-if="orderInfo">
+              <div>收货人 : {{orderInfo.consignee}} {{orderInfo.mobile}}</div>
+              <div
+                class="c-add-ils color6"
+              >收货地址：{{orderInfo.province + orderInfo.city+ orderInfo.district + orderInfo.address}}</div>
             </div>
-            <div v-else>请选择收货地址</div>
           </div>
         </van-cell>
       </van-cell-group>
+    </div>
+    <div class="d-space"></div>
+    <div class="order-list">
+       <div class="o-content2">
+        <div class="left">订单状态 :</div>
+        <div class="right s-price">未付款</div>
+      </div>
+      <div class="o-content">
+        <div class="left">订单编号 :</div>
+        <div class="right">{{orderInfo.orderSn}}</div>
+      </div>
+      <div class="o-content">
+        <div class="left">下单时间 :</div>
+        <div class="right">{{orderInfo.addTime}}</div>
+      </div>
+    </div>
+    <div class="d-space"></div>
+    <div  style="width:100%;">
+      <div class="shop-cart" v-for="(item,index) in orderGoods" :key="index">
+        <div style="padding:10px 15px 0 15px;">
+          <div class="shop-row">
+            <div class="s-r-image">
+              <image :src="item.listPicUrl"/>
+            </div>
+            <div class="s-r-content">
+              <div class="s-r-name">{{item.goodsName}}</div>
+              <div>
+                <span class="s-r-remark">{{item.goodsSpecifitionNameValue}}</span>
+              </div>
+              <div class="s-p-n">
+                <div class="s-p-p">￥{{item.retailPrice}}</div>
+                <div class="s-p-step">
+                  <van-stepper
+                    :value="item.number"
+                    integer="true"
+                    input-class="inputClass"
+                    minus-class="minusClass"
+                    plus-class="plusClass"
+                    step="1"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-space"></div>
+      </div>
+    </div>
+    <div class="order-list">
+      <div class="o-content">
+        <div class="left">支付方式 :</div>
+        <div class="right">微信</div>
+      </div>
+      <div class="o-content">
+        <div class="left">商品合计 :</div>
+        <div class="right">{{orderInfo.goodsPrice}}</div>
+      </div>
+       <div class="o-content">
+        <div class="left">运费 :</div>
+        <div class="right">0.00</div>
+      </div>
+      <div class="o-content2 o-c-s s-price">
+        <div class="left">实付 :</div>
+        <div class="right">{{orderInfo.actualPrice}}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -21,37 +87,140 @@ import { GetOrderDeatilsApi } from "@/utils/http/api.js";
 export default {
   data() {
     return {
-      query:{},
-      orderInfo:{}
+      query: {},
+      orderInfo: {},
+      orderGoods:[]
     };
   },
-  mounted(){
-    this.query=this.$common.getUrlPages()
-    console.log(this.query)
-    if(this.query){
-      this.GetOrderDeatils(this.query.orderId)
+  mounted() {
+    this.query = this.$common.getUrlPages();
+    if (this.query) {
+      this.GetOrderDeatils(this.query.orderId);
     }
   },
-  methods:{
-    GetOrderDeatils(orderId){
-      const that = this
-      const c= res=>{
-        that.orderInfo = res.orderInfo
-      }
-      const param={
-        orderId :orderId
-      }
-      GetOrderDeatilsApi(param).then(c)
+  methods: {
+    GetOrderDeatils(orderId) {
+      const that = this;
+      const c = res => {
+        that.orderInfo = res.orderInfo;
+        that.orderGoods = res.orderGoods
+      };
+      const param = {
+        orderId: orderId
+      };
+      GetOrderDeatilsApi(param).then(c);
     }
   }
 };
 </script>
 
- <style lang="less">.c-address {
+ <style lang="less">
+.c-address {
   width: 100%;
   .c-add-ils {
     line-height: 20px;
     padding-top: 5px;
+  }
+}
+.order-list{
+  background: white;
+  padding: 10px 0;
+  width: 100%;
+  .o-c-s{
+    border-top:1px solid #f5f5f5;
+    padding:5px 0;
+  }
+  .o-c-z{
+    font-size: 14px;
+    border-bottom: 1px solid #f5f5f5;
+  }
+  .o-content2{
+    height: 25px;
+    line-height: 25px;
+    display: flex;
+    font-size: 12px;
+    margin-left:40rpx;
+    .left{
+      width: 100px;
+    }
+    .right{
+      flex: 1;
+    }
+  }
+  .o-content{
+    height: 25px;
+    line-height: 25px;
+    display: flex;
+    font-size: 12px;
+    padding: 0 20px;
+    .left{
+      width: 100px;
+    }
+    .right{
+      flex: 1;
+    }
+  }
+}
+
+.shop-cart {
+  width: 100%;
+  background: white;
+  .shop-row {
+    display: flex;
+    height: 110px;
+    .s-r-checkbox {
+      padding-right: 10px;
+      line-height: 100px;
+    }
+    .s-r-image {
+      padding-right: 10px;
+      image {
+        width: 100px;
+        height: 100px;
+        background: #f5f5f5;
+      }
+    }
+    .s-r-content {
+      height: 23px;
+      line-height: 24px;
+      width: 100%;
+      .s-p-n {
+        display: flex;
+        margin-top: 10rpx;
+        .s-p-p {
+          width: 30%;
+        }
+        .s-p-step {
+          width: 55%;
+        }
+        .s-p-del {
+          width: 15%;
+          font-size: 20px;
+          height: 30px;
+          line-height: 32px;
+          text-align: right;
+          color: #666;
+        }
+      }
+      .s-r-name {
+        min-height: 35px;
+        text-overflow: -o-ellipsis-lastline;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+      }
+      .s-r-remark {
+        width: auto;
+        padding: 2px 5px;
+        background: #f5f5f5;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        margin: 5px 0;
+      }
+    }
   }
 }
 </style>
